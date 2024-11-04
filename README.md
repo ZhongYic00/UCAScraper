113996ff19a6d7832babe6a65ce3e542962f5638
zhongyic00 <zhong200234@outlook.com>
2024-11-05 00:10:22 +0800

    feat: use iframe to serve the upload page
    local server is now unnecessary;
    google cloud app's API_KEY and CLIENT_ID should be set in GM config

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ You can subscribe to this public google calendar in your calendar/email provider
 ![Google Calendar](https://github.com/user-attachments/assets/6e24475d-3c77-465a-b875-3a54ec85d218)
 
 Tested on Outlook, Mi Mailbox.
+
+### Usage
+Preconditions:
+- An google cloud account is required
+- GreaseMonkey is required
+
+Steps:
+- Add this `get-lecture.user.js` script to your GM extension.
+- Enable calendar API according to [JavaScript 快速入门  |  Google Calendar  |  Google for Developers](https://developers.google.com/calendar/api/quickstart/js?hl=zh-cn#next_steps)
+  - Add an item of 'https://xkcts.ucas.ac.cn:8443' in Authorized JavaScript Origins
+- Fill the API_KEY and CLIENT_ID just got in GM script's `data` config.
--- greasemonkey/get-lectures.user.js
+++ greasemonkey/get-lectures.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
-// @name         抓取讲座信息
+// @name         UCAS讲座信息抓取
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  抓取讲座信息并生成JSON 保存到剪贴板
-// @author       RubbishZyc
+// @version      1.1
+// @description  抓取讲座信息并生成JSON到剪贴板，可从页面底部的附加面板上传至google calendar
 // @match        https://xkcts.ucas.ac.cn:8443/subject/lecture
 // @match        https://xkcts.ucas.ac.cn:8443/subject/humanityLecture
-// @grant        GM_download
+// @grant        GM_getValue
 // ==/UserScript==
 
 (function() {
@@ -91,11 +90,286 @@ function assignIgnoringUndefined(target, ...sources) {
         // const csvContent = '讲座名称,讲座时间,详情\n' + data.map(item => `${item.name},${item.time},"${item.details.replace(/"/g, '""')}"`).join('\n');
         // downloadFile(csvContent, 'lectures.csv');
         const jsonstr = JSON.stringify(data);
+        const talksToUpload = jsonstr;
         console.log(jsonstr);
         navigator.clipboard.writeText(jsonstr).then(() => {
             console.log('数据已复制到剪贴板');
             // 打开 t.html
-            window.open('http://localhost/gcalendarSyncLectures.html', '_blank');
+            // window.open('http://localhost:1145/gcalendarSyncLectures.html', '_blank');
+
+          // 创建 iframe 元素
+            const iframe = document.createElement('iframe');
+            iframe.style.width = '100%';  // 设置宽度
+            iframe.style.height = '300px'; // 设置高度
+            iframe.style.border = '1px solid black'; // 设置边框
+            iframe.style.position = 'fixed'; // 固定位置
+            iframe.style.bottom = '10px'; // 距底部 10px
+            iframe.style.left = '10px'; // 距左边 10px
+            iframe.style.zIndex = '2000'; // 确保在其他元素之上
+            iframe.style.background = "white";
+            const CLIENT_ID = GM_getValue('CLIENT_ID','');
+            const API_KEY = GM_getValue('API_KEY');
+            console.log('client',CLIENT_ID,'api',API_KEY);
+
+            // 设定 iframe 的内容
+            const iframeContent = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Google Calendar API Quickstart</title>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <p>Google Calendar API Quickstart</p>
+
+    <div>
+      <button id="paste_btn">paste</button>
+      <p id="pasted" style="height: 2em;overflow: hidden; text-wrap: wrap;"></p>
+    </div>
+    <!--Add buttons to initiate auth sequence and sign out-->
+    <button id="authorize_button" onclick="handleAuthClick()">Authorize</button>
+    <button id="signout_button" onclick="handleSignoutClick()">Sign Out</button>
+
+    <pre id="content" style="white-space: pre-wrap;"></pre>
+
+    <button id="upload_btn" onclick="startUpload()">Start Upload</button>
+
+    <script type="text/javascript">
+      /* exported gapiLoaded */
+      /* exported gisLoaded */
+      /* exported handleAuthClick */
+      /* exported handleSignoutClick */
+
+      // TODO(developer): Set to client ID and API key from the Developer Console
+      let CLIENT_ID = "${CLIENT_ID}";
+      let API_KEY = "${API_KEY}";
+      const UCAS_CALENDARID = '8b1087cb41e1dbaab08c72e1db5f1c3fdc03c814448ffd2b0dcf0b7f62146e3c@group.calendar.google.com';
+
+      // Discovery doc URL for APIs used by the quickstart
+      const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      const SCOPES = 'https://www.googleapis.com/auth/calendar';
+
+      let tokenClient;
+      let gapiInited = false;
+      let gisInited = false;
+      let talkIds = null;
+      let talksToUpload = ${talksToUpload};
+      document.getElementById('pasted').textContent = JSON.stringify(talksToUpload, null, 2);
+
+      document.getElementById('authorize_button').style.visibility = 'hidden';
+      document.getElementById('signout_button').style.visibility = 'hidden';
+      document.getElementById('paste_btn').addEventListener('click', () => {
+          navigator.clipboard.readText().then(text => {
+              const complexData = JSON.parse(text);
+              talksToUpload = complexData;
+              document.getElementById('pasted').textContent = JSON.stringify(complexData, null, 2);
+          }).catch(err => {
+              console.error('读取剪贴板失败:', err);
+          });
+      });
+
+      /**
+       * Callback after api.js is loaded.
+       */
+      async function gapiLoaded() {
+//        const resp = await fetch('.cred.json');
+//        const data = await resp.json();
+//        console.log('Credentials loaded',data);
+//        API_KEY = data.API_KEY;
+//        CLIENT_ID = data.CLIENT_ID;
+        gapi.load('client', initializeGapiClient);
+      }
+
+      /**
+       * Callback after the API client is loaded. Loads the
+       * discovery doc to initialize the API.
+       */
+      async function initializeGapiClient() {
+        await gapi.client.init({
+          apiKey: API_KEY,
+          discoveryDocs: [DISCOVERY_DOC],
+        });
+
+        gapiInited = true;
+        maybeEnableButtons();
+
+        const savedToken = sessionStorage.getItem('googleCalendar_access_token');
+        if (savedToken) {
+          gapi.client.setToken({ access_token: savedToken });
+          afterLogin();
+        }
+      }
+
+      /**
+       * Callback after Google Identity Services are loaded.
+       */
+      function gisLoaded() {
+        tokenClient = google.accounts.oauth2.initTokenClient({
+          client_id: CLIENT_ID,
+          scope: SCOPES,
+          callback: '', // defined later
+        });
+        gisInited = true;
+        maybeEnableButtons();
+      }
+
+      /**
+       * Enables user interaction after all libraries are loaded.
+       */
+      function maybeEnableButtons() {
+        if (gapiInited && gisInited) {
+          document.getElementById('authorize_button').style.visibility = 'visible';
+        }
+      }
+      async function afterLogin() {
+        document.getElementById('signout_button').style.visibility = 'visible';
+        document.getElementById('authorize_button').innerText = 'Refresh';
+        const events = await getEvents();
+        talkIds = new Map(events.map( e => [e.summary, e.id]));
+
+        if (!events || events.length == 0) {
+          document.getElementById('content').innerText = 'No events found.';
+          return;
+        }
+        // Flatten to string to display
+        const output = events.reduce(
+            (str, event) => \`\${str}\${event.summary} (\${event.start.dateTime || event.start.date})\\n\`,
+            'Events:\\n');
+        document.getElementById('content').innerText = output;
+      }
+
+      /**
+       *  Sign in the user upon button click.
+       */
+      function handleAuthClick() {
+        tokenClient.callback = async (resp) => {
+          if (resp.error !== undefined) {
+            throw (resp);
+          }
+          sessionStorage.setItem('googleCalendar_access_token', resp.access_token);
+          await afterLogin();
+        };
+
+        if (gapi.client.getToken() === null) {
+          // Prompt the user to select a Google Account and ask for consent to share their data
+          // when establishing a new session.
+          tokenClient.requestAccessToken({prompt: 'consent'});
+        } else {
+          // Skip display of account chooser and consent dialog for an existing session.
+          tokenClient.requestAccessToken({prompt: ''});
+        }
+      }
+
+      /**
+       *  Sign out the user upon button click.
+       */
+      function handleSignoutClick() {
+        const token = gapi.client.getToken();
+        if (token !== null) {
+          google.accounts.oauth2.revoke(token.access_token);
+          gapi.client.setToken('');
+          document.getElementById('content').innerText = '';
+          document.getElementById('authorize_button').innerText = 'Authorize';
+          document.getElementById('signout_button').style.visibility = 'hidden';
+        }
+      }
+
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+      async function getEvents() {
+        const request = {
+          'calendarId': UCAS_CALENDARID,
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 200,
+          'orderBy': 'startTime',
+        };
+        const response = await gapi.client.calendar.events.list(request);
+        console.log(response);
+        return response.result.items;
+      }
+      function startUpload() {
+        const uploadEvent = async (ev)=>{
+          const request = {
+            'calendarId': UCAS_CALENDARID,
+            'resource': ev,
+          };
+          const resp = await gapi.client.calendar.events.insert(request);
+        }
+        const patchEvent = async (id,changes)=>{
+          await gapi.client.calendar.events.patch({
+            calendarId: UCAS_CALENDARID,
+            eventId: id,
+            changes: changes
+          })
+        }
+        const now = new Date()
+        for (let t of talksToUpload) {
+          if (t.name.includes("待定"))
+            continue;
+          const event = {
+            summary: t.name,
+            start: {
+              dateTime: new Date(t.details.startTime).toISOString(),
+            },
+            end: {
+              dateTime: new Date(t.details.endTime).toISOString(),
+            },
+            location: t.details.mainVenue,
+            description: t.details.introduction,
+            colorId: t.name.includes("明德")? 7 : undefined,
+          }
+          if(new Date(t.details.startTime) < now)
+            continue;
+          if (talkIds.has(event.summary)) {
+            const {summary, ...changes} = event
+            console.log('patch',event)
+            patchEvent(talkIds.get(event.summary), changes)
+          } else {
+            console.log('create',event)
+            uploadEvent(event)
+
+          }
+        }
+      }
+    </script>
+    <script async defer src="https://apis.google.com/js/api.js" onload="gapiLoaded()"></script>
+    <script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script>
+  </body>
+</html>
+            `;
+
+            // 将 iframe 添加到页面中
+            document.body.appendChild(iframe);
+            // 写入内容
+            const doc = iframe.contentDocument || iframe.contentWindow.document;
+            console.log(iframe);
+            console.log(iframeContent);
+            console.log(iframe.contentDocument);
+            // doc.open();
+            doc.write(iframeContent);
+            doc.close();
+
+            const toggleAction = ()=>{
+              console.log('toggle pannel',iframe.style.visibility);
+              iframe.style.visibility=iframe.style.visibility=='hidden'?'visible':'hidden';
+            }
+            const toggle = document.createElement('button');
+            toggle.innerText = 'Toggle Panel';
+            toggle.style.position = 'fixed';
+            toggle.style.bottom = '50px';
+            toggle.style.right = '10px';
+            toggle.style.zIndex = 2001;
+            toggle.onclick = toggleAction;
+            document.body.appendChild(toggle)
+
         }).catch(err => {
             console.error('复制失败:', err);
         });
@@ -104,9 +378,9 @@ function assignIgnoringUndefined(target, ...sources) {
     const button = document.createElement('button');
     button.innerText = '抓取讲座信息';
     button.style.position = 'fixed';
-    button.style.top = '10px';
+    button.style.bottom = '10px';
     button.style.right = '10px';
-    button.style.zIndex = 1000;
+    button.style.zIndex = 2001;
     button.onclick = scrapeTable;
 
     document.body.appendChild(button);
